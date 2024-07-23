6f2c7c49b3cd7acd068daa51d40747423e4497e2
unknown <gmx.butler@gmail.com>
2024-07-23 11:36:10 -0500

    nav bar styling - hover

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,15 +8,15 @@ const NavigationBar = () => {
   const [mode, setMode] = useThemeSwitcher(0);
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 py-8 font-semibold flex items-center text-large justify-between dark:text-light">
       <nav>
-        <Link href={"/"} className="pr-8">
+        <Link href={"/"} className="pr-8 hover:underline hover:underline-offset-2">
           Home
         </Link>
-        <Link href={"/about"} className="pr-8">
+        <Link href={"/about"} className="pr-8 hover:underline hover:underline-offset-2">
           About
         </Link>
-        <Link href={"/projects"} className="pr-8">
+        <Link href={"/projects"} className="pr-8 hover:underline hover:underline-offset-2">
           Projects
         </Link>
       </nav>
@@ -25,14 +25,14 @@ const NavigationBar = () => {
         <Link
           href={"https://www.linkedin.com/in/grace-b-834429188/"}
           target="_blank"
-          className="w-7 mx-1"
+          className="w-7 mx-2"
         >
           <LinkedInIcon />
         </Link>
-        <Link href={"/"} target="_blank" className="w-7 mx-1">
+        <Link href={"/"} target="_blank" className="w-8 ">
           <GithubIcon />
         </Link>
-        <button onClick={() => setMode(mode === "light" ? "dark": "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }`}>
+        <button onClick={() => setMode(mode === "light" ? "dark": "light")} className={`ml-2 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark" }`}>
           {mode === "dark" ? (
             <SunIcon className={"fill-dark"} />
           ) : (
