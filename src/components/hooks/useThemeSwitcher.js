0368aa3cac60fa69007eab201e51f785e3533603
unknown <gmx.butler@gmail.com>
2024-07-26 15:05:42 -0500

    bug fix- Themeing-dark/light switching

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -36,11 +36,11 @@ const useThemeSwitcher = () => {
 
   useEffect(() => {
     if (mode === "dark") {
-      window.localStorage.setItem("theme", "light");
+      window.localStorage.setItem("theme", "dark");
       document.documentElement.classList.add("dark");
     } 
     if(mode == "light") {
-      window.localStorage.setItem("theme", "dark");
+      window.localStorage.setItem("theme", "light");
       document.documentElement.classList.remove("dark");
     }
 
